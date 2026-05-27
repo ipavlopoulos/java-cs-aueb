c403810843259176614cd8affc726c40efeb7410
ipavlopoulos <annis@aueb.gr>
2026-05-27 10:05:52 +0300

    Last updates

---
+++ gr/aueb/cs/inheritance/Final.java
@@ -0,0 +1,27 @@
+package gr.aueb.cs.inheritance;
+
+public class Final {
+    public static void main(String[] args) {
+        A a = new A();
+        a.meth();
+        A b = new B();
+        b.meth();
+    }
+}
+
+class A {
+    final void meth() {
+        System.out.println("This is a final method.");
+    }
+
+    void meth(String a){
+        System.out.println("This is a final method overloaded.");
+    }
+}
+class B extends A {
+
+    @Override
+    void meth(String a){
+        System.out.println("This is a final method.");
+    }
+}
---
+++ gr/aueb/cs/strings/StringPlay.java
@@ -0,0 +1,10 @@
+package gr.aueb.cs.strings;
+
+public class StringPlay {
+    public static void main(String[] args) {
+        String s = "Alice";
+        System.out.println(s);
+        s = "John";
+        System.out.println(s);
+    }
+}
